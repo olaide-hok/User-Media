8023b6c3bbdc39c2742b527fa4f5f4a5ae9e76c4
Habeeb Kareem <olaidekareem43@yahoo.com>
2022-12-22 15:46:41 +0100

    Add extraReducers to usersSlice

--- src/store/slices/usersSlice.js
+++ src/store/slices/usersSlice.js
@@ -1,11 +1,27 @@
 import {createSlice} from '@reduxjs/toolkit'
+import {fetchUsers} from '../thunks/fetchUsers'
 
 const usersSlice = createSlice({
     name: 'users',
     initialState: {
         data: [],
+        isLoading: false,
+        error: null,
     },
-    reducers: {},
+    // Watch for additonal action types/reducers that are not inherently tied to this slice.
+    extraReducers(builder) {
+        builder.addCase(fetchUsers.pending, (state, action) => {
+            state.isLoading = true
+        })
+        builder.addCase(fetchUsers.fulfilled, (state, action) => {
+            state.isLoading = false
+            state.data = action.payload
+        })
+        builder.addCase(fetchUsers.rejected, (state, action) => {
+            state.isLoading = false
+            state.error = action.error
+        })
+    },
 })
 
 export const usersReducer = usersSlice.reducer
