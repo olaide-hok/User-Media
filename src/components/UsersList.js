5021f2be3ebd225b3305a117c63fc8463bd03acc
Habeeb Kareem <olaidekareem43@yahoo.com>
2022-12-23 02:16:48 +0100

    Update UsersList component
    Add return statement for isLoading state.
    
    Add Button component to render add user button to db.

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,6 +1,8 @@
 import {useEffect} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
-import {fetchUsers} from '../store'
+import {addUser, fetchUsers} from '../store'
+import Skeleton from './Skeleton'
+import Button from './Button'
 
 function UsersList() {
     const dispatch = useDispatch()
@@ -11,14 +13,37 @@ function UsersList() {
         dispatch(fetchUsers())
     }, [dispatch])
 
+    const handleUserAdd = () => {
+        dispatch(addUser())
+    }
+
     if (isLoading) {
-        return <div>Loading...</div>
+        return <Skeleton times={6} className="h-10 w-full" />
     }
 
     if (error) {
         return <div>Error fetching data...</div>
     }
-    return 'Users List'
+
+    const renderedUsers = data.map((user) => {
+        return (
+            <div key={user.id} className="mb-2 border rounded">
+                <div className="flex p-2 justify-between items-center cursor-pointer">
+                    {user.name}
+                </div>
+            </div>
+        )
+    })
+
+    return (
+        <div>
+            <div className="flex flex-row justify-between m-3">
+                <h1 className="m-2 text-xl">Users</h1>
+                <Button onClick={handleUserAdd}>+ Add User</Button>
+            </div>
+            {renderedUsers}
+        </div>
+    )
 }
 
 export default UsersList
