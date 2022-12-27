1ce4ec70b279b08066146b219436a68308217b34
Habeeb Kareem <olaidekareem43@yahoo.com>
2022-12-27 17:17:52 +0100

    Refactored AlbumsList component.
    Render skeleton component for loading state.
    
    Render text content for error state.
    
    Render add album button

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,9 +1,41 @@
 import React from 'react'
-import {useFetchAlbumsQuery} from '../store'
+import {useAddAlbumMutation, useFetchAlbumsQuery} from '../store'
+import Skeleton from './Skeleton'
+import ExpandablePanel from './ExpandablePanel'
+import Button from './Button'
 
 const AlbumsList = ({user}) => {
     const {data, error, isLoading} = useFetchAlbumsQuery(user)
-    return <div>Albums for {user.name}</div>
+    const [addAlbum, results] = useAddAlbumMutation()
+
+    const handleAddAlbum = () => {
+        addAlbum(user)
+    }
+
+    let content
+    if (isLoading) {
+        content = <Skeleton times={3} />
+    } else if (error) {
+        content = <div>Error loading albums.</div>
+    } else {
+        content = data.map((album) => {
+            const header = <div>{album.title}</div>
+            return (
+                <ExpandablePanel key={album.id} header={header}>
+                    List of photos in the album
+                </ExpandablePanel>
+            )
+        })
+    }
+    return (
+        <div>
+            <div>
+                Albums for {user.name}
+                <Button onClick={handleAddAlbum}>+ Add Album</Button>
+            </div>
+            <div>{content}</div>
+        </div>
+    )
 }
 
 export default AlbumsList
