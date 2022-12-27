cbc4bc29c12972b03f62ddc6cff42c47de9ca37d
Habeeb Kareem <olaidekareem43@yahoo.com>
2022-12-27 15:05:42 +0100

    Add hook to fetch users albums list.

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import {useFetchAlbumsQuery} from '../store'
 
 const AlbumsList = ({user}) => {
+    const {data, error, isLoading} = useFetchAlbumsQuery(user)
     return <div>Albums for {user.name}</div>
 }
 
