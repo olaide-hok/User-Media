348a802c4dfa91f5da5319fb2d20823afaa3da35
Habeeb Kareem <olaidekareem43@yahoo.com>
2022-12-27 09:15:55 +0100

    Refactored UsersListItem component
    Renders AlbumsList and Expandable component.

--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -2,7 +2,9 @@ import React from 'react'
 import {useThunk} from '../hooks/use-thunk'
 import {removeUser} from '../store'
 import Button from './Button'
+import ExpandablePanel from './ExpandablePanel'
 import {GoTrashcan} from 'react-icons/go'
+import AlbumsList from './AlbumsList'
 
 const UsersListItem = ({user}) => {
     const [doRemoveUser, isLoading, error] = useThunk(removeUser)
@@ -10,21 +12,20 @@ const UsersListItem = ({user}) => {
     const handleClick = () => {
         doRemoveUser(user)
     }
+
+    const header = (
+        <>
+            <Button className="mr-3" loading={isLoading} onClick={handleClick}>
+                <GoTrashcan />
+            </Button>
+            {error && <div>Error deleting user.</div>}
+            {user.name}
+        </>
+    )
     return (
-        <div className="mb-2 border rounded">
-            <div className="flex p-2 justify-between items-center cursor-pointer">
-                <div className="flex flex-row items-center justify-between">
-                    <Button
-                        className="mr-3"
-                        loading={isLoading}
-                        onClick={handleClick}>
-                        <GoTrashcan />
-                    </Button>
-                    {error && <div>Error deleting user.</div>}
-                    {user.name}
-                </div>
-            </div>
-        </div>
+        <ExpandablePanel header={header}>
+            <AlbumsList user={user} />
+        </ExpandablePanel>
     )
 }
 
