c53d8f14eb756ae133384074d0289e85449b0be8
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-01-02 20:46:28 +0100

    Add tagTypes to albumsApi.
    Add invalidatesTags to addAlbum mutation.
    
    Add providesTags to fetchAlbums query.
    
    Add the pause fn to slow down fetch request to the JSON server.
    
    Modify in-built fetch fn with RTKQ fetchFn.

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -1,14 +1,31 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 import {faker} from '@faker-js/faker'
 
+// Dev only!!!
+const pause = (duration) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, duration)
+    })
+}
+
 const albumsApi = createApi({
     reducerPath: 'albums',
+    tagTypes: 'Album',
     baseQuery: fetchBaseQuery({
         baseUrl: 'http://localhost:3005',
+        // FetchFn allows for overriding built-in fetch function
+        fetchFn: async (...args) => {
+            //Remove for production
+            await pause(2000)
+            return fetch(...args)
+        },
     }),
     endpoints(builder) {
         return {
             addAlbum: builder.mutation({
+                invalidatesTags: (result, error, user) => {
+                    return [{type: 'Album', id: user.id}]
+                },
                 query: (user) => {
                     // user is a args/parameter for the useAddAlbumMutation hook i.e. useAddAlbumMutation(user)
                     return {
@@ -22,6 +39,9 @@ const albumsApi = createApi({
                 },
             }),
             fetchAlbums: builder.query({
+                providesTags: (result, error, user) => {
+                    return [{type: 'Album', id: user.id}]
+                },
                 // fetchAlbums automatically generates a useFetchAlbums hook
                 query: (user) => {
                     // user args is what is passed into the useFetchAlbums hook i.e. useFetchAlbums(user)
