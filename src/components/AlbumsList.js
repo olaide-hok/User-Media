8b415bb7ca6a4a85eaf42aa5479d517ad473fe08
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-01-02 20:41:54 +0100

    Refactor AlbumsList component.
    Render AlbumsListItem component.
    
    Add css classnames to Skeleton component.
    
    Refactor add album button to show spinner when in loading state.

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import {useAddAlbumMutation, useFetchAlbumsQuery} from '../store'
 import Skeleton from './Skeleton'
-import ExpandablePanel from './ExpandablePanel'
 import Button from './Button'
+import AlbumsListItem from './AlbumsListItem'
 
 const AlbumsList = ({user}) => {
     const {data, error, isLoading} = useFetchAlbumsQuery(user)
@@ -14,24 +14,21 @@ const AlbumsList = ({user}) => {
 
     let content
     if (isLoading) {
-        content = <Skeleton times={3} />
+        content = <Skeleton className="h-10 w-full" times={3} />
     } else if (error) {
         content = <div>Error loading albums.</div>
     } else {
         content = data.map((album) => {
-            const header = <div>{album.title}</div>
-            return (
-                <ExpandablePanel key={album.id} header={header}>
-                    List of photos in the album
-                </ExpandablePanel>
-            )
+            return <AlbumsListItem key={album.id} album={album} />
         })
     }
     return (
         <div>
-            <div>
-                Albums for {user.name}
-                <Button onClick={handleAddAlbum}>+ Add Album</Button>
+            <div className="m-2 flex flex-row items-center justify-between">
+                <h3 className="text-lg font-bold"> Albums for {user.name}</h3>
+                <Button loading={results.isLoading} onClick={handleAddAlbum}>
+                    + Add Album
+                </Button>
             </div>
             <div>{content}</div>
         </div>
