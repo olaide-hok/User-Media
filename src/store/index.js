47b3a78c68811b4fe2c6459b9f6d6ae86373e325
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-01-03 16:57:35 +0100

    Export photosApi custom generated hooks.

--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,18 @@ import {configureStore} from '@reduxjs/toolkit'
 import {usersReducer} from './slices/usersSlice'
 import {setupListeners} from '@reduxjs/toolkit/dist/query'
 import {albumsApi} from './apis/albumsApi'
+import {photosApi} from './apis/photosApi'
 
 export const store = configureStore({
     reducer: {
         users: usersReducer,
         [albumsApi.reducerPath]: albumsApi.reducer, // [albumsApi.reducerPath] === albums
+        [photosApi.reducerPath]: photosApi.reducer, // [photosApi.reducerPath] === photos
     },
     middleware: (getDefaultMiddleware) => {
-        return getDefaultMiddleware().concat(albumsApi.middleware)
+        return getDefaultMiddleware()
+            .concat(albumsApi.middleware)
+            .concat(photosApi.middleware)
     },
 })
 
@@ -24,3 +28,9 @@ export {
     useAddAlbumMutation,
     useRemoveAlbumMutation,
 } from './apis/albumsApi'
+
+export {
+    useFetchPhotosQuery,
+    useAddPhotoMutation,
+    useRemovePhotoMutation,
+} from './apis/photosApi'
