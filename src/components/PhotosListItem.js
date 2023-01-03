741144ce4fbdf7aafdb42a836068d732ed067411
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-01-03 16:53:24 +0100

    Create PhotosListItem component.
    Renders a single photo to the UI.
    
    Uses the useRemovePhotoMutation hook to remove photos from the database.

---
+++ src/components/PhotosListItem.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {useRemovePhotoMutation} from '../store'
+import {GoTrashcan} from 'react-icons/go'
+
+const PhotosListItem = ({photo}) => {
+    const [removePhoto] = useRemovePhotoMutation()
+    const handleRemovePhoto = () => {
+        removePhoto(photo)
+    }
+
+    return (
+        <div
+            onClick={handleRemovePhoto}
+            className="relative cursor-pointer m-2">
+            <img className="h-20 w-20" src={photo.url} alt="random pick" />
+            <div className="absolute inset-0 flex items-center justify-center hover:bg-gray-200 opacity-0 hover:opacity-80">
+                <GoTrashcan className="text-3xl" />
+            </div>
+        </div>
+    )
+}
+
+export default PhotosListItem
